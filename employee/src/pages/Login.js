bdae6b8e27b51cba9350d73eec5ce93655a17337
Kumar Utkarsh <kr.utkarsh106@gmail.com>
2021-06-06 02:20:48 +0530

    Added_pages

---
+++ employee/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <input name="email" type="email" placeholder="Email"/>
+            <input name="password" type="email" placeholder="Password"/>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login
---
+++ employee/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <div>
+            <input type="text" placeholder="Enter name"/>
+            <input type="email" placeholder="Email"/>
+            <input type="email" placeholder="Password"/>
+            <button>Save</button>
+            <button>Close</button>
+        </div>
+    )
+}
+
+export default Signup
